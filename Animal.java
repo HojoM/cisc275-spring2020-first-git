42ae33f61c741f64223d4a7a9c1b6bdd059bb529
HojoM <aidenmax187@gmail.com>
2020-03-01 20:45:59 -0500

    first commit

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+        String name;
+        int legs;
+        public Animal(String name, int legs) {
+                this.name = name;
+                this.legs = legs;
+        }
+        public int getLegs() {
+                return this.legs;
+        }
+}
